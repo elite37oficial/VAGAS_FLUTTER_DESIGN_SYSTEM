659cbea2c4df6ac70223116c47298cbb3d114991
Gustavo Henrick <gustavo.napolitano@ws-solution.com>
2023-03-08 13:19:49 -0300

    feat: Created Responsive Text and Core Pages

---
+++ lib/src/pages/error_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:vagas_design_system/src/widgets/responsive_text_widget.dart';
+
+class ErrorPage extends StatefulWidget {
+  const ErrorPage({Key? key}) : super(key: key);
+
+  @override
+  State<ErrorPage> createState() => _ErrorPageState();
+}
+
+class _ErrorPageState extends State<ErrorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: ResponsiveTextWidget(
+          text: "404",
+          textStyle: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w700,
+            fontSize: 27,
+          ),
+        ),
+      ),
+    );
+  }
+}
