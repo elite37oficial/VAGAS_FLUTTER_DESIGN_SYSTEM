de724a27f3042425798f82c5ceb75924136e4b6e
Gustavo Henrick <gustavo.napolitano@ws-solution.com>
2023-03-20 16:14:50 -0300

    fix: Fixed AppWebField Widget

--- lib/src/widgets/fields/app_web_field_widget.dart
+++ lib/src/widgets/fields/app_web_field_widget.dart
@@ -13,7 +13,6 @@ class AppWebFieldWidget extends StatefulWidget {
   final String? labelSemantic;
   final String? labelTooltip;
   final String? hint;
-  final String? labelText;
   final TextStyle? hintStyle;
   final String? hintSemantic;
   final String? fieldSemantic;
@@ -33,7 +32,6 @@ class AppWebFieldWidget extends StatefulWidget {
     Key? key,
     this.prefixIcon,
     this.label,
-    this.labelText,
     this.labelStyle,
     this.labelSemantic,
     this.labelTooltip,
@@ -61,125 +59,136 @@ class AppWebFieldWidget extends StatefulWidget {
 }
 
 class _AppWebFieldWidgetState extends State<AppWebFieldWidget> {
-  bool obscure = true;
+  bool _obscure = true;
+  final _focusNode = FocusNode();
+
+  Color _borderColor = AppColors.grey;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        widget.label == null
-            ? const SizedBox.shrink()
-            : ResponsiveTextWidget(
-                text: widget.label ?? "",
-                textStyle: widget.labelStyle,
-                hintSemantics: widget.labelSemantic,
-                tooltipSemantics: widget.labelTooltip,
-              ),
-        widget.label == null
-            ? const SizedBox.shrink()
-            : const SizedBox(height: 5),
-        Semantics(
-          textField: true,
-          container: true,
-          hint: widget.hintSemantic,
-          tooltip: widget.fieldSemantic,
-          value: widget.hintSemantic,
-          label: widget.hintSemantic,
-          enabled: true,
-          hidden: false,
-          obscured: obscure,
-          increasedValue: widget.hintSemantic,
-          tagForChildren: SemanticsTag(widget.hintSemantic ?? "textfield"),
-          child: Container(
-            constraints: widget.constraints,
-            height: widget.heigth,
-            width: widget.width,
-            child: TextFormField(
-              readOnly: widget.readyOnly,
-              obscureText: widget.isPassword ? obscure : false,
-              validator: widget.validator,
-              controller: widget.controller,
-              inputFormatters: widget.inputFormatters,
-              onChanged: widget.onChanged,
-              onFieldSubmitted: widget.onFieldSubmitted,
-              keyboardType: widget.inputType,
-              cursorColor: widget.onError ? AppColors.red : AppColors.grey,
-              cursorHeight: 18,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.normal,
-                color: AppColors.grey,
-              ),
-              decoration: InputDecoration(
-                labelText: widget.labelText,
-                constraints: widget.constraints,
-                hintText: widget.hint,
-                hintStyle: widget.hintStyle ??
-                    const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.normal,
-                      color: AppColors.lightGrey,
+    return Focus(
+      focusNode: _focusNode,
+      onFocusChange: (hasFocus) {
+        if (hasFocus) {
+          setState(() {
+            _borderColor = AppColors.accentBlue;
+          });
+        } else {
+          setState(() {
+            _borderColor = widget.onError ? AppColors.red : AppColors.grey;
+          });
+        }
+      },
+      child: Semantics(
+        textField: true,
+        container: true,
+        hint: widget.hintSemantic,
+        tooltip: widget.fieldSemantic,
+        value: widget.hintSemantic,
+        label: widget.hintSemantic,
+        enabled: true,
+        hidden: false,
+        obscured: _obscure,
+        increasedValue: widget.hintSemantic,
+        tagForChildren: SemanticsTag(widget.hintSemantic ?? "textfield"),
+        child: Container(
+          constraints: widget.constraints,
+          decoration: BoxDecoration(
+            border: Border.all(
+                color: widget.onError ? AppColors.red : _borderColor, width: 1),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          padding: const EdgeInsets.only(top: 5),
+          height: widget.heigth,
+          width: widget.width,
+          child: TextFormField(
+            readOnly: widget.readyOnly,
+            obscureText: widget.isPassword ? _obscure : false,
+            validator: widget.validator,
+            controller: widget.controller,
+            inputFormatters: widget.inputFormatters,
+            onChanged: widget.onChanged,
+            onFieldSubmitted: widget.onFieldSubmitted,
+            keyboardType: widget.inputType,
+            cursorColor: widget.onError ? AppColors.red : _borderColor,
+            cursorHeight: 18,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.normal,
+              color: AppColors.grey,
+            ),
+            decoration: InputDecoration(
+              focusColor: AppColors.accentBlue,
+              border: InputBorder.none,
+              focusedBorder: InputBorder.none,
+              errorBorder: InputBorder.none,
+              label: widget.label == null
+                  ? null
+                  : ResponsiveTextWidget(
+                      text: widget.label ?? "",
+                      textStyle: widget.labelStyle == null
+                          ? TextStyle(
+                              color:
+                                  widget.onError ? AppColors.red : _borderColor,
+                            )
+                          : widget.labelStyle!.copyWith(
+                              color:
+                                  widget.onError ? AppColors.red : _borderColor,
+                            ),
+                      hintSemantics: widget.labelSemantic,
+                      tooltipSemantics: widget.labelTooltip,
                     ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                      width: 1,
-                      color: widget.onError ? AppColors.red : AppColors.grey),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                      width: 1,
-                      color: widget.onError ? AppColors.red : AppColors.grey),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(width: 1, color: AppColors.grey),
-                ),
-                prefixIcon: widget.prefixIcon,
-                contentPadding:
-                    const EdgeInsets.only(bottom: 5, left: 10, right: 10),
-                suffixIcon: widget.isPassword
-                    ? Semantics(
-                        button: true,
-                        hint: "Botão de Visualizar Senha",
-                        tooltip: "Visualizar Senha",
-                        value: "Visualizar Senha",
-                        label: "Botão de Visualizar Senha",
-                        hidden: false,
-                        enabled: true,
-                        obscured: false,
-                        increasedValue: widget.hintSemantic,
-                        tagForChildren: const SemanticsTag("button"),
-                        child: GestureDetector(
-                          onTap: () => setState(() => obscure = !obscure),
-                          child: Padding(
-                            padding: const EdgeInsets.only(right: 20),
-                            child: SizedBox(
-                              height: widget.heigth == null
-                                  ? 40
-                                  : widget.heigth! * 0.75,
-                              width: 20,
-                              child: Center(
-                                child: FaIcon(
-                                  obscure
-                                      ? FontAwesomeIcons.eyeSlash
-                                      : FontAwesomeIcons.eye,
-                                  color: widget.onError
-                                      ? AppColors.red
-                                      : AppColors.lightGrey,
-                                ),
+              constraints: widget.constraints,
+              hintText: widget.hint,
+              hintStyle: widget.hintStyle ??
+                  const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                    color: AppColors.lightGrey,
+                  ),
+              prefixIcon: widget.prefixIcon,
+              contentPadding:
+                  const EdgeInsets.only(bottom: 5, left: 10, right: 10),
+              suffixIcon: widget.isPassword
+                  ? Semantics(
+                      button: true,
+                      hint: "Botão de Visualizar Senha",
+                      tooltip: "Visualizar Senha",
+                      value: "Visualizar Senha",
+                      label: "Botão de Visualizar Senha",
+                      hidden: false,
+                      enabled: true,
+                      obscured: false,
+                      increasedValue: widget.hintSemantic,
+                      tagForChildren: const SemanticsTag("button"),
+                      child: GestureDetector(
+                        onTap: () => setState(() => _obscure = !_obscure),
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: SizedBox(
+                            height: widget.heigth == null
+                                ? 40
+                                : widget.heigth! * 0.75,
+                            width: 20,
+                            child: Center(
+                              child: FaIcon(
+                                _obscure
+                                    ? FontAwesomeIcons.eyeSlash
+                                    : FontAwesomeIcons.eye,
+                                color: widget.onError
+                                    ? AppColors.red
+                                    : AppColors.lightGrey,
                               ),
                             ),
                           ),
                         ),
-                      )
-                    : null,
-              ),
+                      ),
+                    )
+                  : null,
             ),
           ),
         ),
-      ],
+      ),
     );
   }
 }
