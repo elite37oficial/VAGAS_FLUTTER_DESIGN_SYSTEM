df14dfa888bc7b322839e11d61b54d59972dfc54
Gustavo Henrick <gustavo.napolitano@ws-solution.com>
2023-04-04 19:19:23 -0300

    fix: Fixed Export Buttons

--- lib/src/widgets/widgets_export.dart
+++ lib/src/widgets/widgets_export.dart
@@ -2,3 +2,5 @@ export 'texts/responsive_text_widget.dart';
 export 'fields/app_web_field_widget.dart';
 export 'buttons/social_circle_button_widget.dart';
 export 'app_bar/top_bar_web_widget.dart';
+export 'buttons/outlined_button_widget.dart';
+export 'buttons/page_button_widget.dart';
