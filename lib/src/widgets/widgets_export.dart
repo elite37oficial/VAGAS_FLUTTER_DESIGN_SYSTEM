e59b30101498dd1cc54e04dcbe53ca078b2ee301
Gustavo Henrick <gustavo.napolitano@ws-solution.com>
2023-04-04 17:04:41 -0300

    feat: Export TopBarWeb Widget

--- lib/src/widgets/widgets_export.dart
+++ lib/src/widgets/widgets_export.dart
@@ -1,3 +1,4 @@
 export 'texts/responsive_text_widget.dart';
 export 'fields/app_web_field_widget.dart';
 export 'buttons/social_circle_button_widget.dart';
+export 'app_bar/top_bar_web_widget.dart';
