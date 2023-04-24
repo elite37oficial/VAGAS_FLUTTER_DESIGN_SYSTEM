f6740b30b50011739bee369196011d6845c395ce
Gustavo Henrick <gustavo.napolitano@ws-solution.com>
2023-04-24 17:01:36 -0300

    feat: Created AppMenuDropdownWidget

--- lib/src/widgets/menus/app_menu_dropdown_widget.dart
+++ lib/src/widgets/menus/app_menu_dropdown_widget.dart
@@ -1,47 +1,31 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/semantics.dart';
 import 'package:vagas_design_system/vagas_design_system.dart';
 
 class AppMenuDropDownWidget extends StatefulWidget {
-  final double? fontSize;
+  final String label;
+  TextEditingController controller;
+  final String hint;
+  final String semantics;
+  final Function(String?) onChanged;
+  final List<DropdownMenuItem<String>> listItens;
+  bool error;
+  bool onlyDropdown;
+  final bool isEnable;
+  final double height;
 
-  final Widget? prefixIcon;
-  final String? label;
-  final TextStyle? labelStyle;
-  final String? labelSemantic;
-  final String? labelTooltip;
-  final String? hint;
-  final TextStyle? hintStyle;
-  final String? hintSemantic;
-  final String? fieldSemantic;
-  final double? heigth;
-  final double? width;
-  final TextEditingController controller;
-  final Function(String)? onChanged;
-  final bool onError;
-  final BoxConstraints? constraints;
-  final String? Function(String?)? validator;
-  final List<DropdownMenuItem<String>>? listOptions;
-  const AppMenuDropDownWidget({
+  AppMenuDropDownWidget({
     Key? key,
-    this.fontSize,
-    this.prefixIcon,
-    this.label,
-    this.labelStyle,
-    this.labelSemantic,
-    this.labelTooltip,
-    this.hint,
-    this.hintStyle,
-    this.hintSemantic,
-    this.fieldSemantic,
-    this.heigth,
-    this.width,
+    required this.height,
+    required this.semantics,
     required this.controller,
-    this.onChanged,
-    required this.onError,
-    this.constraints,
-    this.validator,
-    required this.listOptions,
+    required this.hint,
+    required this.label,
+    required this.listItens,
+    required this.onChanged,
+    required this.error,
+    this.onlyDropdown = false,
+    this.isEnable = true,
   }) : super(key: key);
 
   @override
@@ -49,72 +33,109 @@ class AppMenuDropDownWidget extends StatefulWidget {
 }
 
 class _AppMenuDropDownWidgetState extends State<AppMenuDropDownWidget> {
-  final Color _borderColor = AppColors.black.withOpacity(0.5);
+  final _focusNode = FocusNode();
+
+  Color _borderColor = AppColors.black.withOpacity(0.5);
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Semantics(
-      textField: true,
-      container: true,
-      hint: widget.hintSemantic,
-      tooltip: widget.fieldSemantic,
-      value: widget.hintSemantic,
-      label: widget.hintSemantic,
-      enabled: true,
-      hidden: false,
-      increasedValue: widget.hintSemantic,
-      tagForChildren: SemanticsTag(widget.hintSemantic ?? "textfield"),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          widget.label == null
-              ? const SizedBox.shrink()
-              : Padding(
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: ResponsiveTextWidget(
-                    text: widget.label ?? "",
-                    maxLines: 1,
-                    maxFontSize: widget.fontSize,
-                    textStyle: Theme.of(context).textTheme.bodySmall!.copyWith(
-                          color: AppColors.black,
-                          fontWeight: FontWeight.w700,
+    return Focus(
+      focusNode: _focusNode,
+      onFocusChange: (hasFocus) {
+        if (hasFocus) {
+          setState(() {
+            _borderColor = AppColors.black;
+          });
+        } else {
+          setState(() {
+            _borderColor =
+                widget.error ? AppColors.red : AppColors.black.withOpacity(0.5);
+          });
+        }
+      },
+      child: Semantics(
+        hint: widget.semantics,
+        tooltip: widget.semantics,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            !widget.onlyDropdown
+                ? Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: ResponsiveTextWidget(
+                      text: widget.label,
+                      textStyle: Theme.of(context)
+                          .textTheme
+                          .bodySmall!
+                          .copyWith(
+                            fontWeight: FontWeight.w700,
+                            color:
+                                widget.error ? AppColors.red : AppColors.black,
+                          ),
+                    ),
+                  )
+                : Container(),
+            widget.listItens.isEmpty
+                ? Container(
+                    height: widget.height,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: _borderColor)),
+                    child:
+                        Center(child: ResponsiveTextWidget(text: widget.hint)),
+                  )
+                : Container(
+                    height: widget.height,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                            color:
+                                widget.error ? AppColors.red : _borderColor)),
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButton2(
+                        items: widget.listItens,
+                        hint: ResponsiveTextWidget(
+                          text: widget.hint,
+                          textStyle:
+                              Theme.of(context).textTheme.bodyMedium?.copyWith(
+                                    color: AppColors.grey,
+                                  ),
+                          maxLines: 1,
+                          maxFontSize: 22,
+                          minFontSize: 16,
+                          hintSemantics: "hint",
+                          tooltipSemantics: "hint",
                         ),
-                    hintSemantics: widget.label,
-                    tooltipSemantics: widget.label,
+                        iconStyleData: const IconStyleData(
+                            icon: Padding(
+                              padding: EdgeInsets.only(right: 10),
+                              child: Icon(
+                                Icons.keyboard_arrow_down_rounded,
+                                color: AppColors.black,
+                              ),
+                            ),
+                            openMenuIcon: Padding(
+                              padding: EdgeInsets.only(right: 10),
+                              child: Icon(
+                                Icons.keyboard_arrow_up_rounded,
+                                color: AppColors.black,
+                              ),
+                            )),
+                        onChanged: widget.onChanged,
+                        isExpanded: true,
+                        value: widget.controller.text.isEmpty
+                            ? null
+                            : widget.controller.text,
+                      ),
+                    ),
                   ),
-                ),
-          Container(
-            constraints: widget.constraints,
-            decoration: BoxDecoration(
-              border: Border.all(
-                  color: widget.onError ? AppColors.red : _borderColor,
-                  width: 1),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            padding: const EdgeInsets.only(top: 5),
-            height: widget.heigth,
-            width: widget.width,
-            child: DropdownButton(
-              items: widget.listOptions,
-              onChanged: (value) => setState(() => widget.controller.text),
-              isExpanded: true,
-              value: widget.controller.text,
-              underline: null,
-              enableFeedback: true,
-              icon: const Icon(
-                Icons.keyboard_arrow_down_rounded,
-                color: AppColors.black,
-              ),
-              hint: ResponsiveTextWidget(
-                text: widget.hint ?? "",
-              ),
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(10),
-                bottomRight: Radius.circular(10),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
